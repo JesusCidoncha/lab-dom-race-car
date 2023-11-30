3da4f7aa8c5954ecb38b87d3a00382760e4aa324
Jesus Cidoncha <jesus.cidoncha.berlanga@gmail.com>
2023-11-30 15:52:28 +0100

    full cars lab

--- js/game.js
+++ js/game.js
@@ -1,3 +1,59 @@
 class Game {
-    // code to be added
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.height = 600;
+    this.width = 500;
+    this.player = null;
+    this.obstacles = [];
+    this.animateId = null;
+    this.score = 0;
+    this.lives = 3;
+    this.isGameOver = false;
+  }
+  start() {
+    this.startScreen.style.display = "none";
+    this.gameScreen.style.display = "block";
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+    this.player = new Player(this.gameScreen);
+    this.gameLoop();
+  }
+  gameLoop() {
+    this.player.move();
+
+    const nextObstacles = [];
+    this.obstacles.forEach((currentObstacle) => {
+      currentObstacle.move();
+      if (currentObstacle.top < 640) {
+        if (this.player.didCollide(currentObstacle)) {
+          this.lives -= 1;
+          document.getElementById("lives").innerText = `${this.lives}`;
+          currentObstacle.element.remove();
+
+          if (this.lives === 0) {
+            this.isGameOver = true;
+            this.gameScreen.style.display = "none";
+            this.gameEndScreen.style.display = "block";
+          }
+        } else {
+          nextObstacles.push(currentObstacle);
+        }
+      } else {
+        this.score += 10;
+        document.getElementById("score").innerText = `${this.score}`;
+        currentObstacle.element.remove();
+      }
+    });
+    this.obstacles = nextObstacles;
+
+    if (this.animateId % 300 === 0) {
+      this.obstacles.push(new Obstacle(this.gameScreen));
+    }
+
+    this.animateId = requestAnimationFrame(() => {
+      this.gameLoop();
+    });
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,61 @@
-window.onload = function () {
+window.addEventListener("load", () => {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  const game = new Game();
+  function startGame() {
+    console.log("start game");
+    game.start();
+  }
   startButton.addEventListener("click", function () {
     startGame();
   });
+  restartButton.addEventListener("click", function () {
+    // Call the restartGame function when the button is clicked
+    restartGame();
+  });
 
-  function startGame() {
-    console.log("start game");
+  // The function that reloads the page to start a new game
+  function restartGame() {
+    location.reload();
   }
-};
+
+  document.addEventListener("keydown", (event) => {
+    event.preventDefault();
+    if (event.code === "ArrowUp" || event.code === "KeyW") {
+      game.player.directionY = -1;
+    }
+    if (event.code === "ArrowDown" || event.code === "KeyS") {
+      console.log("go down");
+      game.player.directionY = 1;
+    }
+    if (event.code === "ArrowLeft" || event.code === "KeyA") {
+      console.log("go left");
+      game.player.directionX = -1;
+    }
+    if (event.code === "ArrowRight" || event.code === "KeyD") {
+      console.log("go right");
+      game.player.directionX = 1;
+    }
+  });
+  document.addEventListener("keyup", (event) => {
+    if (
+      event.code === "ArrowUp" ||
+      event.code === "KeyW" ||
+      event.code === "ArrowDown" ||
+      event.code === "KeyS"
+    ) {
+      console.log("go up");
+      game.player.directionY = 0;
+    }
+
+    if (
+      event.code === "ArrowLeft" ||
+      event.code === "KeyA" ||
+      event.code === "ArrowRight" ||
+      event.code === "KeyD"
+    ) {
+      console.log("go left");
+      game.player.directionX = 0;
+    }
+  });
+});
